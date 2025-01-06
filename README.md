34fd66b4bdad91ec528c05e85a7763ee122cfc4f
LauraForde <lauraforde@outlook.com>
2025-01-06 17:37:34 +0800

    - Second Task
    - Reading two numbers in from the user and doing basic maths

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ I will be using this repo to track tasks I have asked ChatGPT to generate to aid
 1. Hello, World!
     * Write a program to write "Hello, World!" to the console.
     * Modify it to accept a user input and print "Hello, []!".
+2. Basic Arithemtic Calculator
+    * Create a program that takes two numbers from the user and preforms basic operations (addition, subtraction, multiplication, division).
---
+++ beginner_tasks/b2.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main(){
+	// Take in two numbers
+	var first int
+	var second int
+	
+	fmt.Println("Enter your two numbers: ")
+	fmt.Scanln(&first, &second)
+	
+	// Addition
+	add := first + second
+	fmt.Println("Addition: ", first, " + ", second, " = ", add)
+
+	// Subtraction
+	sub := first - second
+	fmt.Println("Subtraction: ", first, " - ", second, " = ", sub)
+
+	// Multiplication
+	mul := first * second
+	fmt.Println("Multiplication: ", first, " * ", second, " = ", mul)
+
+	// Division
+	div := first / second
+	fmt.Println("Division: ", first, " / ", second, " = ", div)
+}
