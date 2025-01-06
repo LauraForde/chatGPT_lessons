de98a4470ef78ea3e0f522b03d33de81bd45d573
LauraForde <lauraforde@outlook.com>
2025-01-06 17:18:10 +0800

    - Take user input
    - Amending the code from the first task to take input from the user to
      say hello to them

--- beginner_tasks/b1.go
+++ beginner_tasks/b1.go
@@ -4,5 +4,11 @@ import "fmt"
 
 func main(){
 	// Task 1
-	fmt.Println("Hello, World!")
+	//fmt.Println("Hello, World!")
+
+	// Task 2
+	var name string
+	fmt.Println("Enter your name: ")
+	fmt.Scanln(&name)
+	fmt.Println("Hello, " + name + "!")
 }
