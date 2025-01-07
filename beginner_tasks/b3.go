ffd1e7af047c672d321c306f03bc0c49ad90a752
LauraForde <lauraforde@outlook.com>
2025-01-07 11:33:13 +0800

    - Using for/switch
    - Doing FizzBuzz using a for switch

--- beginner_tasks/b3.go
+++ beginner_tasks/b3.go
@@ -5,7 +5,8 @@ import "fmt"
 func main(){
 	// Task 3 FizzBuzz
 
-	for i := 1; i <= 100; i++{
+	// Using for/if loops
+	/*for i := 1; i <= 100; i++{
 		
 		if i % 5 == 0 && i % 3 == 0{
 			fmt.Println("FizzBuzz")
@@ -16,6 +17,19 @@ func main(){
 		} else{
 			fmt.Println(i)
 		}
+	}*/
+
+	// Using switch
+	for i := 1; i <= 100; i++{
+		switch{
+		case(i % 5 == 0 && i % 3 == 0):
+			fmt.Println("FizzBuzz")
+		case(i % 3 == 0):
+			fmt.Println("Fizz")
+		case(i % 5 == 0):
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+		}
 	}
-	
 }
