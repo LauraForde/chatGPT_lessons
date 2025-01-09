fd3573d1766c7f4cdaa7f3509e362dfcd60c80ab
LauraForde <lauraforde@outlook.com>
2025-01-09 18:18:59 +0800

    - Switch on user input
    - Adding in a switch after asking the user which beginner program they
      want to run

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/LauraForde/chatGPT_lessons/beginner_tasks"
+import (
+	"fmt"
+
+	"github.com/LauraForde/chatGPT_lessons/beginner_tasks"
+)
 
 var content = []string{
 	"Hello World.",
@@ -9,10 +13,31 @@ var content = []string{
 
 func main() {
 
-	beginner_tasks.B1()
-	beginner_tasks.B2()
-	beginner_tasks.B3()
-	beginner_tasks.B4()
-	beginner_tasks.B5()
+	var option int
+
+	fmt.Println("Choose one of the following:" + "\n" + "1. Hello, World" + "\n" + "2. Basic Arithmetic Calculator" + "\n" + "3. FizzBuzz" +
+		"\n" + "4. Temperature Converter" + "\n" + "5. Read and Write File")
+	fmt.Scanf("%d", &option)
+
+	switch {
+	case option == 1:
+		// Hello World
+		beginner_tasks.B1()
+	case option == 2:
+		// Basic Arithmetic Calculator
+		beginner_tasks.B2()
+	case option == 3:
+		// FizzBuzz
+		beginner_tasks.B3()
+	case option == 4:
+		// Temperature Converter
+		beginner_tasks.B4()
+	case option == 5:
+		// Read and Write Files
+		beginner_tasks.B5()
+	default:
+		fmt.Println("Invalid option")
+		return
+	}
 
 }
